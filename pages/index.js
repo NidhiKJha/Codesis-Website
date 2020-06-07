b0ad41112020ebf9b4f3e73125e350a7b022515c
Nidhi Kumari <nidhi1691996@outlook.com>
2020-06-07 22:49:06 +0530

    gatsby boiler plate code

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Test from "../components/index"
+import "./style.css"
+
+export default function Home() {
+  return <div className= "main">
+    <Test/>
+  </div>
+}
